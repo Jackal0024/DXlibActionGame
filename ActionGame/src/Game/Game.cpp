00ba012761ed1d75351451d8708f07d350ef6c7e
jac kal <jac kal>
2016-05-15 23:04:42 +0900

    VECTORのオペレーション処置

--- ActionGame/src/Game/Game.cpp
+++ ActionGame/src/Game/Game.cpp
@@ -23,8 +23,10 @@ void Game::Update()
 
 void Game::Draw()
 {
+	VECTOR v1 = VGet(1.0f,0.0f,0.0f);
+	VECTOR v2 = VGet(1.0f, 0.0f, 0.0f);
 	const char* str;
-	std::string st = std::to_string(Input::getInstance().GetRightAnalogStick().y);
+	std::string st = std::to_string((v1+v2).x);
 	str = st.data();
 	DrawString(250, 240 - 32, str, GetColor(255, 255, 255));
 }
